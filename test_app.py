3d9076d3dbd514cd3aabcbe75f60e9d9ea438071
manishambule0234 <156496342+manishambule0234@users.noreply.github.com>
2024-01-20 05:56:42 +0000

    Pending changes exported from your codespace

---
+++ test_app.py
@@ -0,0 +1,22 @@
+import app
+import json
+import warnings
+
+def test_hello_world():
+    response = app.app.test_client().get('/hello')
+    assert response.status_code == 200
+    assert response.data == b'Hello'
+
+def test_goodbye():
+    response = app.app.test_client().get('/goodbye')
+    assert response.status_code == 404
+
+def test_name():
+    response = app.app.test_client().get('/name')
+    assert response.status_code == 200
+    assert response.data == b'Manish'
+
+def test_age():
+    response = app.app.test_client().get('/age')
+    assert response.status_code == 200
+    assert response.text == '20'
