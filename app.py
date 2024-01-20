b2bd831a57326f4e033f599594c73da4a034876f
manishambule0234 <156496342+manishambule0234@users.noreply.github.com>
2024-01-20 06:00:58 +0000

    Flask

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/name")
+def name():
+    return "Manish"
+
+@app.route("/age")
+def age():
+    return "20"
+
+@app.route("/hello")
+def hello():
+    return "Hello"
